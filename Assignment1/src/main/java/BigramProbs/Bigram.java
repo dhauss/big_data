618063bb6a14ba5a68483006645342f97b92d4f4
dhauss <davidkhauss@gmail.com>
2023-09-29 10:36:56 -0400

    Bigram class implemented

--- Assignment1/src/main/java/BigramProbs/Bigram.java
+++ Assignment1/src/main/java/BigramProbs/Bigram.java
@@ -4,22 +4,80 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
 
-public class Bigram implements Writable{
-	private String first;
-	private String second;
+public class Bigram implements WritableComparable<Bigram>{
+	private Text first;
+	private Text second;
+
+	public Bigram() {
+		this.first = new Text();
+		this.second = new Text();
+	}
+
+	public Bigram(Text first, Text second) {
+		this.first = first;
+		this.second = second;
+	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
-		// TODO Auto-generated method stub
-		
+		this.first.write(out);
+		this.second.write(out);
 	}
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
-		// TODO Auto-generated method stub
-		
+		this.first.readFields(in);
+		this.second.readFields(in);
 	}
 
+	@Override
+	public int compareTo(Bigram other) {
+		int compFirst = first.compareTo(other.first);
+		if (compFirst != 0) {
+			return compFirst;
+		}
+		else {
+			return second.compareTo(other.second);
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return first.hashCode() * 113 + second.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if(other instanceof Bigram) {
+			Bigram otherBG = (Bigram) other;
+			return first.equals(otherBG.first) && second.equals(otherBG.second);
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return this.first.toString() + "\t" + this.second.toString();
+	}
+
+	public Text getFirst() {
+		return first;
+	}
+
+	public Text getSecond() {
+		return second;
+	}
+
+	public void setFirst(Text first) {
+		this.first = first;
+	}
+
+	public void setSecond(Text second) {
+		this.second = second;
+	}
 }
