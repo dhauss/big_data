02828bcec1bda2043453369363ead58f105a3693
dhauss <davidkhauss@gmail.com>
2023-10-05 14:58:09 -0400

    explicitly made attributes private, parser replaces non-alphanumeric chars to null

--- Assignment1/src/main/java/bigram/probs/CountMapper.java
+++ Assignment1/src/main/java/bigram/probs/CountMapper.java
@@ -7,9 +7,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class CountMapper extends Mapper<LongWritable, Text, Bigram, LongWritable>{
-
-	Text lastWord = null;
-	Text curWord = new Text();
+	private Text lastWord = null;
+	private Text curWord = new Text();
     private final static LongWritable one = new LongWritable(1);
 
     public void map(LongWritable key, Text value, Context context)
@@ -18,7 +17,7 @@ public class CountMapper extends Mapper<LongWritable, Text, Bigram, LongWritable
     	//normalize to lower case, remove all html tags, remove punctuation, and remove consecutive spaces
     	String line = value.toString().toLowerCase();
     	line = line.replaceAll("\\<.*?\\>", "");
-    	line = line.replaceAll("[^\\sa-z0-9]", " ");
+    	line = line.replaceAll("[^\\sa-z0-9]", "");
     	line = line.replaceAll("\\s{2,}", " ");
 
     	String[] words = line.split(" ");
