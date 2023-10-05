0346d1eca20c9edc3fe55f2e3c25a77c49c9b414
dhauss <davidkhauss@gmail.com>
2023-10-05 14:59:28 -0400

    added outKey attribute

--- Assignment1/src/main/java/bigram/probs/ProbMapper.java
+++ Assignment1/src/main/java/bigram/probs/ProbMapper.java
@@ -6,10 +6,10 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class ProbMapper extends Mapper<LongWritable, Text, Bigram, FloatWritable>{
 	private long bigramCount;
+	private Bigram outKey = null;
 	
 	@Override
 	protected void setup(Context context) throws IOException, InterruptedException {
@@ -20,7 +20,7 @@ public class ProbMapper extends Mapper<LongWritable, Text, Bigram, FloatWritable
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
     	String line = value.toString();
     	String[] vals = line.split("\t");
-    	Bigram outKey = new Bigram(vals[0], vals[1]);
+    	outKey = new Bigram(vals[0], vals[1]);
     	float outVal = Float.parseFloat(vals[2]) / bigramCount;
 		context.write(outKey, new FloatWritable(outVal));
     }
