f5a3083363b58373e53078c0a8c314e43380ea2e
dhauss <davidkhauss@gmail.com>
2023-10-05 10:18:11 -0400

    removed unused imports

--- Assignment1/src/main/java/bigram/probs/PossibleMapper.java
+++ Assignment1/src/main/java/bigram/probs/PossibleMapper.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class PossibleMapper extends Mapper<LongWritable, Text, Text, Text> {
 	
