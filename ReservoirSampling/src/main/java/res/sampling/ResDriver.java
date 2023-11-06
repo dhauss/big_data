3d0b76d5e03dd77e664a733248081d9c365dd9dd
dhauss <davidkhauss@gmail.com>
2023-11-06 13:55:55 -0500

    resMapper implemented, mapper only driver implemented

--- ReservoirSampling/src/main/java/res/sampling/ResDriver.java
+++ ReservoirSampling/src/main/java/res/sampling/ResDriver.java
@@ -1,5 +1,34 @@
 package res.sampling;
 
-public class ResDriver {
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+
+public class ResDriver extends Configured implements Tool{
+	
+	public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new ResDriver(), args);
+        System.exit(exitCode);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+        Job resJob = Job.getInstance(getConf());
+        resJob.setJobName("ReservoirSampling");
+        resJob.setJarByClass(ResDriver.class);
+        
+        resJob.setMapperClass(ResMapper.class);
+        resJob.setNumReduceTasks(0);
+
+        resJob.setOutputKeyClass(LongWritable.class);
+        resJob.setOutputValueClass(Text.class);
+
+        return resJob.waitForCompletion(true) ? 0 : 1;
+	}
 
 }
--- ReservoirSampling/src/main/java/res/sampling/ResMapper.java
+++ ReservoirSampling/src/main/java/res/sampling/ResMapper.java
@@ -5,7 +5,55 @@ import org.apache.hadoop.io.Text;
 
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+import java.util.Random;
+
 
 public class ResMapper extends Mapper<LongWritable, Text, LongWritable, Text>{
-
+	//size of random sample
+	static final int K = 10000;
+	//array to save current random sample in memory
+	String[] reservoir = new String[K];
+	//Random object for computing j, which determines if an element will replace a reservoir entry
+	Random rand = new Random();
+	
+    public void map(LongWritable key, Text value, Context context) {
+    	//split csv file by line breaks to create array of rows
+    	String[] rows = value.toString().split("\n");
+    	
+    	int i = 0;
+    	//iterate through input, apply reservoir sampling algo to fill reservoir
+    	for(String row: rows) {
+    		//initially fill reservoir with first K elements
+    		if(i < K) {
+    			reservoir[i] = new String(row);
+    			i++;
+    		} else {
+    		//compute random int, j, to decide if ith element will replace jth element in reservoir
+    			int j = rand.nextInt(i);
+    			if(j < K) {
+    				reservoir[j] = new String(row);
+    			}
+    			i++;
+    		}
+    	}
+    	
+    	// write final reservoir to output
+    	for(String out: reservoir) {
+    		// if reservoir is not filled, avoid writing null entries to context
+    		if(out == null) {
+    			break;
+    		} else {
+    			try {
+    				//arbitrary key of 1 to send all data to single reducer (output size limited to number of mappers * K)
+    				//replaces multiple commas with single comma for output readability given plethora of null entries in dataset
+    				context.write(new LongWritable(1), new Text(out.replaceAll(",{2,}", ",")));
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			} catch (InterruptedException e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	}
+    }
 }
