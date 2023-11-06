c41c731e908af95244b76f927ed5064f6107896c
dhauss <davidkhauss@gmail.com>
2023-11-06 13:56:49 -0500

    simple mapper test implemented

---
+++ ReservoirSampling/src/test/java/res/test/TestReservoirSampling.java
@@ -0,0 +1,35 @@
+package res.test;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import res.sampling.ResMapper;
+
+public class TestReservoirSampling {
+    MapDriver<LongWritable, Text, LongWritable, Text> mapDriver;
+    
+    @Before
+    public void setUp() {
+    	//countJob Mapper
+    	ResMapper mapper = new ResMapper();
+        mapDriver = new MapDriver<LongWritable, Text, LongWritable, Text>();
+        mapDriver.setMapper(mapper);
+    }
+    
+    @Test
+    public void testMapper() throws IOException{
+    	mapDriver.withInput(new LongWritable(1), new Text("some,entry\nother,,,,entry"));
+    	mapDriver.withOutput(new LongWritable(1), new Text("some,entry"));
+    	mapDriver.withOutput(new LongWritable(1), new Text("other,entry"));
+    	mapDriver.runTest();
+    }
+
+
+}
