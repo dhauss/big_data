7197d60b09ec66a7e38c7dd05f9f4134944109d8
dhauss <davidkhauss@gmail.com>
2023-11-07 11:24:11 -0500

    comments and code formatting

--- ReservoirSampling/src/main/java/res/sampling/ResDriver.java
+++ ReservoirSampling/src/main/java/res/sampling/ResDriver.java
@@ -16,7 +16,6 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 
-
 public class ResDriver extends Configured implements Tool{
 	
 	public static void main(String[] args) throws Exception {
@@ -31,24 +30,24 @@ public class ResDriver extends Configured implements Tool{
             System.err.println("Usage: <input path> <output path>");
             return -1;
         }
-		
+
         Job resJob = Job.getInstance(getConf());
         resJob.setJobName("ReservoirSampling");
         resJob.setJarByClass(ResDriver.class);
-        
+
         resJob.setMapperClass(ResMapper.class);
         resJob.setReducerClass(ResReducer.class);
+        //only 1 reducer needed for job, set to avoid overhead of inactive reducers and empty output files
         resJob.setNumReduceTasks(1);
 
         resJob.setMapOutputKeyClass(NullWritable.class);
         resJob.setMapOutputValueClass(Text.class);
         resJob.setOutputKeyClass(IntWritable.class);
         resJob.setOutputValueClass(Text.class);
-        
+
         FileInputFormat.addInputPath(resJob, new Path(args[0]));
         FileOutputFormat.setOutputPath(resJob, new Path(args[1]));
 
         return resJob.waitForCompletion(true) ? 0 : 1;
 	}
-
 }
